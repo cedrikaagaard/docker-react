c198e9ce467207a6ebd806af0b8940a693f30bc0
Cedrik Aagaard <cedrikaagaard@gmail.com>
2020-11-07 23:50:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am Cedrik!
         </a>
       </header>
     </div>
