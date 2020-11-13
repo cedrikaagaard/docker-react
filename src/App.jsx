bdc5b20e0627331b09f0adb7c0a7f928d6ce95b6
Cedrik Aagaard <cedrikaagaard@gmail.com>
2020-11-13 11:50:17 +0100

    Fixed JSX and tests

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am Cedrik!
+          react app
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/react app/i);
+    expect(linkElement).toBeInTheDocument();
+});
